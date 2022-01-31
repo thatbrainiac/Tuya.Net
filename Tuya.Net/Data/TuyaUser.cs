d87070d7f8b20a08b53c96a3f33b25087e6248dd
thatbrainiac <tarmac2k[at]gmail[dot]com>
2022-01-31 22:13:50 +0200

    Updated DTOs

--- Tuya.Net/Data/TuyaUser.cs
+++ Tuya.Net/Data/TuyaUser.cs
@@ -8,6 +8,12 @@ namespace Tuya.Net.Data
     public class TuyaUser
     {
         /// <summary>
+        /// Gets or sets the user name.
+        /// </summary>
+        [JsonProperty("username")]
+        public string? Name { get; set; }
+
+        /// <summary>
         /// Gets or sets the avatar url.
         /// </summary>
         [JsonProperty("avatar")]
@@ -25,28 +31,46 @@ namespace Tuya.Net.Data
         [JsonProperty("create_time")]
         public long? CreateTime { get; set; }
 
+        /// <summary>
+        /// Gets or sets the user email.
+        /// </summary>
         [JsonProperty("email")]
         public string? Email { get; set; }
 
+        /// <summary>
+        /// Gets or sets the user mobile.
+        /// </summary>
         [JsonProperty("mobile")]
         public string? Mobile { get; set; }
 
+        /// <summary>
+        /// Gets or sets the nickname.
+        /// </summary>
         [JsonProperty("nick_name")]
         public string? Nickname { get; set; }
 
+        /// <summary>
+        /// Gets or sets the temperature unit.
+        /// </summary>
         [JsonProperty("temp_unit")]
         public long TempUnit { get; set; }
 
+        /// <summary>
+        /// Gets or sets the time zone id.
+        /// </summary>
         [JsonProperty("time_zone_id")]
         public string? TimeZoneId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the UID.
+        /// </summary>
         [JsonProperty("uid")]
         public string? Uid { get; set; }
 
+        /// <summary>
+        /// Gets or sets the update time.
+        /// </summary>
         [JsonProperty("update_time")]
         public long? UpdateTime { get; set; }
-
-        [JsonProperty("username")]
-        public string? Username { get; set; }
     }
 }
